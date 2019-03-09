130ff14ab19b1ca8dd5b418a42c176143ce54b66
visualstudio <visualstudio@122-PC09>
2019-03-08 20:30:18 -0500

    lab 2

--- DelegatesAndEvents/Program.cs
+++ DelegatesAndEvents/Program.cs
@@ -4,35 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DelegatesAndEvents
 //Harpreet Kaur(C0726031)
 //Simranjeet Kaur(C0727100)
 
-
+namespace DelegatesAndEvents
 {
     class Program
     {
         static void Main(string[] args)
-        
-
-            {
-                DelegateExercises The = new DelegateExercises();
-                The.Method2();
-            }
-
-        }
-        public class DelegateExercises
         {
-            public delegate void MyDelegate();
-            void Method1()
-            {
-                Console.WriteLine("Method1");
-                Console.ReadLine();
-            }
-            public void Method2()
-            {
-                MyDelegate myDelegate = new MyDelegate(Method1);
-                myDelegate();
-            }
+            DelegateExercises The = new DelegateExercises();
+            The.Method2(50);
         }
     }
+    public class DelegateExercises
+    {
+        public delegate void MyDelegate(int i);
+        public void Method1(int i)
+        {
+            Console.WriteLine("Method1");
+            Console.WriteLine(i);
+            Console.ReadLine();
+        }
+        public void Method2(int i)
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate(50);
+        }
+    }
+}
