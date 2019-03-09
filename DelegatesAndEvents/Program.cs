574da53ddcaee08013591501c975d4db91498cf6
visualstudio <visualstudio@122-PC09>
2019-03-08 21:27:24 -0500

    Lab 12

--- DelegatesAndEvents/Program.cs
+++ DelegatesAndEvents/Program.cs
@@ -9,26 +9,62 @@ using System.Threading.Tasks;
 
 namespace DelegatesAndEvents
 {
+    using System;
+    delegate void ExampleDelegate(string xyz);
     class Program
     {
-        public static void Main(string[] args)
-        {
-            DelegateExercises the = new DelegateExercises();
-            the.Method3();
-            Console.Read();
-        }
-    }
-    public delegate void MyDelegate();
-    public class DelegateExercises
-    {
-        public void Method1()
+        public static void Method1(string xyz)
+
         {
 
+            Console.WriteLine(xyz + " Method1");
+
         }
-        public void Method3()
+
+        public static void Method2(string xyz)
+
         {
-            MyDelegate myDelegate = new MyDelegate(Method1);
-            System.Console.WriteLine(myDelegate.ToString());
+
+            Console.WriteLine(xyz + " Method2");
+
+        }
+
+        public static void Main()
+
+        {
+
+            ExampleDelegate ex1Delegate, ex2Delegate, ex3Delegate, myDelegate;
+
+            ex1Delegate = new ExampleDelegate(Method1);
+
+            ex2Delegate = new ExampleDelegate(Method2);
+
+            ex3Delegate = ex1Delegate + ex2Delegate;
+
+            myDelegate = ex1Delegate - ex2Delegate;
+
+            ex1Delegate("AAA");
+
+
+
+            ex2Delegate("BBB");
+
+            ex3Delegate("CCC");
+
+            myDelegate("DDD");
+
+            myDelegate = ex3Delegate - ex1Delegate;
+
+            myDelegate("EEE");
+
+            myDelegate = ex3Delegate - ex2Delegate;
+
+            myDelegate("FFF");
+
+            Console.ReadLine();
+
+
+
         }
     }
 
